13991c2bb278cce0b0b256e38aa9c47ecbd21c77
KrakenTW <ziolus03@gmail.com>
2021-05-15 01:26:11 +0200

    Merge branch 'Pierwszy-init' of https://github.com/KrakenTW/JSGRAK into Pierwszy-init

--- Scene1.js
+++ Scene1.js
@@ -1,3 +1,7 @@
+"use strict";
+/*var Gra = Gra || {};
+Gra.launcher = function (game){};
+*/
 
 class Scene1 extends Phaser.Scene {
     constructor() {
@@ -5,12 +9,34 @@ class Scene1 extends Phaser.Scene {
     }
   
     preload(){
-      this.load.image("background", "http://skybots.net/drop-view/skybots_purple-background.png-viewInvertGBR");
-      this.load.image("ship", "https://www.pinclipart.com/picdir/middle/260-2603418_statek-kosmiczny-icon-clipart.png");
+      
+      this.load.video("liquid", "/assets/skull.mp4");
+      this.load.image("background", "/assets/back.webp");
+      this.load.image("backgroundstart", "/assets/green.jpg");
+      this.load.image("astro1","/assets/—Pngtree—futuristic user interface illustration_5439293.png");
+      this.load.image("astro2","/assets/astro1.png");
+      this.load.image("colors","/assets/images/colors.png");
+      //this.load.image("back","/assets/image");
+      this.load.video("woodenback","/assets/zGM5b2.webp");
+      //this.load.image("ship3","/assets/images/");
+      //this.load.image("ship2","/assets/images/");
+      this.load.image("ship", "/assets/images/ship.png");
+      
+      this.load.image('beball', '/assets/images/ship.png');
+      this.load.image('atari', '/assets/images/ship.png');
+      this.load.image('bikkuriman', '/assets/images/ship.png');
     }
-  
+
     create() {
-      this.add.text(20, 20, "Loading game...");
-      this.scene.start("playGame");
+      this.backgroundstart = this.add.image(0, 0, "backgroundstart"); 
+      this.backgroundstart.setOrigin(0, 0);
+      this.backgroundstart.setScale(1);
+      
+      //this.scene.start("Spaceships");
+      this.input.keyboard.once('keydown_SPACE', this.start, this);
+      this.input.once('pointerdown', this.start, this);
+    }
+    start(){
+      this.scene.start("Start-Menu");
     }
   }
---
+++ scenes/PreloadScene.js
@@ -0,0 +1,28 @@
+class PreloadScene extends Phaser.Scene {
+
+	constructor() {
+		super({key : 'preloadScene'});
+	}
+
+	preload() {
+
+		this.load.image('background', 'images/tut/background.png');
+		for(var i =0;i<30;i++) {
+			this.load.image('background_'+i, 'images/tut/background.png');
+		}
+
+		this.load.on('progress', this.updateBar);
+		this.load.on('complete', this.complete);
+	}
+
+	updateBar(percentage) {
+		console.log("P:" + percentage);
+	}
+
+	complete() {
+		console.log("COMPLETE!");
+	}
+
+}
+
+export default PreloadScene;
